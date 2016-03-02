adf9c81c4f47d4d7c90a9ad10d44f73fa10c21d7
Linda Zou <lindazou@Lindas-MacBook-Pro.local>
2016-03-02 15:28:06 -0800

    renders map properly with added css

--- foodZen/client/app/map/map.js
+++ foodZen/client/app/map/map.js
@@ -1,19 +1,7 @@
-// angular.module('foodZen.map', [])
-// // .directive('MapController', function() {
-// 	.config(function(uiGmapGoogleMapApiProvider) {
-// 	    uiGmapGoogleMapApiProvider.configure({
-// 	        //    key: 'your api key',
-// 	        v: '3.20', //defaults to latest 3.X anyhow
-// 	        libraries: 'weather,geometry,visualization'
-// 	    });
-// 	})
-// });
 angular.module("foodZen.map", [])
+
 .controller("MapController", function($scope) {
-  $scope.hitEnter = function($event, input) {
-    if($event.which === 13) {
-    	$scope.addInput(input);
-    }
-  };
+  
 });
 
+ 
